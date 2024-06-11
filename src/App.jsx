9bbcd97960de41c94396ac7c5776925623a73508
john9223 <95772706+john9223@users.noreply.github.com>
2024-06-11 17:48:09 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormComponent from './FormComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Hook Form </h1>
+      <FormComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FormComponent.jsx
@@ -0,0 +1,81 @@
+// src/FormComponent.jsx
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import './FormComponent.css';
+
+// Form field component
+const FormField = ({ id, label, register, validation, error }) => (
+  <div>
+    <label htmlFor={id}>{label}</label>
+    <input id={id} {...register(id, validation)} />
+    {error && <p>{error.message}</p>}
+  </div>
+);
+
+const FormComponent = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormField
+        id="firstName"
+        label="First Name"
+        register={register}
+        validation={{ required: 'First name is required' }}
+        error={errors.firstName}
+      />
+
+      <FormField
+        id="lastName"
+        label="Last Name"
+        register={register}
+        validation={{ required: 'Last name is required' }}
+        error={errors.lastName}
+      />
+
+      <FormField
+        id="email"
+        label="Email"
+        register={register}
+        validation={{
+          required: 'Email is required',
+          pattern: {
+            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            message: 'Invalid email',
+          },
+        }}
+        error={errors.email}
+      />
+
+      <FormField
+        id="password"
+        label="Password"
+        register={register}
+        validation={{
+          required: 'Password is required',
+          minLength: {
+            value: 5,
+            message: 'Password must be more than 4 characters',
+          },
+          maxLength: {
+            value: 20,
+            message: 'Password cannot be more than 20 characters',
+          },
+        }}
+        error={errors.password}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default FormComponent;
